092cdc1657b7ccf2ed2af9f5f8a07418bf88ade3
KerlyGranoble <km.granoblet@uea.edu.ec>
2026-02-01 11:09:00 -0500

    Menu
    Menu agenda

--- Semana4/obj/Debug/net10.0/Semana4.AssemblyInfo.cs
+++ Semana4/obj/Debug/net10.0/Semana4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402ce01b626a9517785d48e102668f9cda32a875")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dff09b32eeb6ef61f9b41c35a6f358a1dcc22139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
